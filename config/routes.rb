f582be230af28699526426a32028eeafb05a8139
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2015-03-05 15:57:52 +0200

    Updated High Voltage routing.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'high_voltage/pages#show', id: 'home'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
