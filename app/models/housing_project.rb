fbf4744ba6290ce0a68d0049ceba4691d111252c
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2015-03-12 14:17:55 +0200

    Updated Plot, User and Housing Project models.

--- app/models/housing_project.rb
+++ app/models/housing_project.rb
@@ -1,2 +1,14 @@
 class HousingProject < ActiveRecord::Base
+  has_many :plots
+
+  serialize :gps_coordinates
+  serialize :plot_layout_image_dimensions
+
+  def image_map_width
+    self.plot_layout_image_dimensions.first
+  end
+
+  def image_map_height
+    self.plot_layout_image_dimensions.last
+  end
 end
