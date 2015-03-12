d63b916459fd054459dd2e1a92ca8d0475552f6c
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2015-03-12 10:03:27 +0200

    Housing Projects scaffold.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :housing_projects
+
   resources :plots
 
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150312075340) do
+ActiveRecord::Schema.define(version: 20150312080132) do
+
+  create_table "housing_projects", force: :cascade do |t|
+    t.string   "cover_image"
+    t.string   "gps_coordinates"
+    t.string   "name"
+    t.string   "plot_layout_image"
+    t.string   "plot_layout_image_dimensions"
+    t.string   "state"
+    t.string   "subdomain"
+    t.text     "summary"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+  end
 
   create_table "plots", force: :cascade do |t|
     t.integer  "housing_project_id"
