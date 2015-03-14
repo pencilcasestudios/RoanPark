f1b869f09c7fe9f5ee8a0c466b5c8f5a108a75fc
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2015-03-14 09:45:04 +0200

    Updated tests. Updated externalised image assets. All tests pass now.

--- spec/requests/manage_assets_spec.rb
+++ spec/requests/manage_assets_spec.rb
@@ -16,6 +16,14 @@ describe "Image assets" do
 		expect(File.exist?("app/assets/images/apple-touch-icon-72x72.png")).to be true
 		expect(File.exist?("app/assets/images/apple-touch-icon-114x114.png")).to be true
 
+
+		# Logos
+		expect(File.exist?("app/assets/images/logos/roan_park_logo-397x332.png")).to be true
+		expect(File.exist?("app/assets/images/logos/roan_park_logo-397x397.png")).to be true
+		expect(File.exist?("app/assets/images/logos/roan_park_logo-400x335.png")).to be true
+		expect(File.exist?("app/assets/images/logos/roan_park_logo-400x400.png")).to be true
+
+
 		# Slides
 		expect(File.exist?("app/assets/images/slides/1.jpg")).to be true
 		expect(File.exist?("app/assets/images/slides/2.jpg")).to be true
